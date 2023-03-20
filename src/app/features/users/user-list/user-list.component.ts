236363c568d1f38e88f44711444c95ef3d057b68
Christian Lu <christian.lu@filamsoftware.com>
2023-03-20 14:46:47 +0800

    UI for User List

--- src/app/features/users/user-list/user-list.component.ts
+++ src/app/features/users/user-list/user-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormItem } from '../../../shared/components/form-controls';
+import { InitColumn } from '../../../shared/components/table';
+import { UserListTableColumns } from './user-list';
 
 @Component({
   selector: 'app-user-list',
@@ -6,7 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  constructor() {}
+  searchMemberField: FormItem = { controlName: 'searchMember', label: 'Name', isSearchField: true };
+  filterCycleField: FormItem = { controlName: 'filterCycle', label: 'Select a Cycle' };
+  filterSubscriptionField: FormItem = { controlName: 'filterSubscription', label: 'Select a Subsciption' };
 
-  ngOnInit(): void {}
+  searchFormGroup: FormGroup = new FormGroup({});
+
+  displayedColumnNames: string[] = [];
+  initColumn: InitColumn[] = [];
+
+  constructor(private cdref: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.setUserListTableProperties();
+    this.setSearchFormGroup();
+  }
+
+  setUserListTableProperties(): void {
+    this.initColumn = UserListTableColumns;
+    this.displayedColumnNames = this.initColumn.map(i => i.name);
+
+  }
+
+  setSearchFormGroup(): void {
+    this.searchFormGroup.addControl(this.searchMemberField.controlName, new FormControl(''));
+    this.searchFormGroup.addControl(this.filterCycleField.controlName, new FormControl(''));
+    this.searchFormGroup.addControl(this.filterSubscriptionField.controlName, new FormControl(''));
+
+    this.cdref.detectChanges();
+
+
+  }
 }
---
+++ src/app/features/users/user-list/user-list.ts
@@ -0,0 +1,13 @@
+import { InitColumn } from "../../../shared/components/table";
+
+export const UserListTableColumns: InitColumn[] = [
+  { name: 'name', displayName: 'Name' },
+  { name: 'age', displayName: 'Age' },
+  { name: 'gender', displayName: 'Gender' },
+  { name: 'country', displayName: 'Country' },
+  { name: 'startDate', displayName: 'Start Date' },
+  { name: 'cycle', displayName: 'Cycle' },
+  { name: 'subscription', displayName: 'Subscription' },
+  { name: 'status', displayName: 'Status' },
+  { name: 'action', displayName: '' }
+];
