bcb655aa2b80f792b79f1ee56023667d9c739079
Cherry Santos <cherry.santos@filamsoftware.com>
2023-03-21 13:22:37 +0800

    update environment import in config service

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AppConfig } from '../models/app-config';
-import { environment } from '../../../environments/environment.prod';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AppConfig } from '../models/app-config';
 
 @Injectable({
   providedIn: 'root'
